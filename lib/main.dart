9301adc75f0d859b2771a89890011e5b1886c8f2
joaopedro742 <joao.boaventura1402@gmail.com>
2025-05-19 15:43:29 -0400

    .

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal.shade700,
         appBar: AppBar(
@@ -16,8 +17,29 @@ void main() {
 }
 
 class Dadoos extends StatelessWidget {
+  const Dadoos({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('imagens/dado1.png'),
+              )),
+              Expanded(child: Padding(  
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('imagens/dado1.png'),
+              )),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
